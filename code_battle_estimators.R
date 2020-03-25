4c1893e9cd3bc0d10641730bd537427c9dbbd5ad
Arnald Puy <arnald.puy@gmail.com>
2020-03-25 09:05:16 -0400

    Added Lamboni and Glen and Isaacs

--- code_battle_estimators.R
+++ code_battle_estimators.R
@@ -66,7 +66,8 @@ savage_scores <- function(x) {
 sobol_Ti <- function(d, N, params, total) {
   m <- matrix(d, nrow = N)
   k <- length(params)
-  if(!total == "azzini") {
+  if(total == "jansen" | total == "homma" | total == "sobol" | total == "monod" |
+     total == "glen") {
     Y_A <- m[, 1]
     Y_AB <- m[, -1]
     f0 <- (1 / length(Y_A)) * sum(Y_A)
@@ -85,15 +86,25 @@ sobol_Ti <- function(d, N, params, total) {
                   (1/ N * Rfast::colsums((Y_A + Y_AB) / 2)) ^ 2) / 
       (1 / N * Rfast::colsums((Y_A ^ 2 + Y_AB ^ 2) / 2) - 
          (1/ N * Rfast::colsums((Y_A + Y_AB) / 2)) ^ 2)
+  } else if(total == "glen") {
+    Ti <- 1 - (1 / (N - 1) * 
+                 Rfast::colsums(((Y_A - mean(Y_A)) * (Y_AB - Rfast::colmeans(Y_AB))) / 
+                                  sqrt(var(Y_A) * Rfast::colVars(Y_AB))))
   }
-  if(total == "azzini") {
+  if(total == "azzini" | total == "lamboni") {
     Y_A <- m[, 1]
     Y_B <- m[, 2]
     Y_AB <- m[, 3:(3 + k - 1)]
     Y_BA <- m[, (ncol(m) - k + 1):ncol(m)]
+    f0 <- 1 / (2 * N) * sum(Y_A + Y_B)
+    VY <- 1 / (2 * N - 1) * sum((Y_A - f0) ^ 2 + (Y_B - f0) ^ 2)
+  }
+  if(total == "azzini") {
     Ti <- 1 - abs(Rfast::colsums((Y_A - Y_BA) * (Y_B - Y_AB)) / 
-                     (1 / 2 * Rfast::colsums((Y_A - Y_B) ^ 2 + (Y_AB - Y_BA) ^ 2)))
-  } 
+                    (1 / 2 * Rfast::colsums((Y_A - Y_B) ^ 2 + (Y_AB - Y_BA) ^ 2)))
+  } else if(total == "lamboni") {
+    Ti <- (1 / (4 * N) * Rfast::colsums((Y_A - Y_AB) ^ 2 + (Y_B - Y_BA) ^ 2)) / VY
+    }
   output <- data.table(Ti)
   output[, `:=`(parameters = paste("X", 1:k, sep = ""))]
   return(output)
@@ -105,7 +116,7 @@ sobol_Ti <- function(d, N, params, total) {
 # CHECK THAT ALL TI ESTIMATORS WORK ------------------------------------------------
 
 # Settings
-estimators <- c("jansen", "sobol", "homma", "azzini", "monod")
+estimators <- c("jansen", "sobol", "homma", "azzini", "monod", "lamboni", "glen")
 test_functions <- c("Ishigami", "Sobol'G", "Morris")
 N <- 2^9
 
@@ -113,7 +124,7 @@ N <- 2^9
 ind <- Y <- mt <- list() 
 for(i in estimators) {
   for(j in test_functions) {
-    if(!i == "azzini") {
+    if(i == "jansen" | i == "sobol" | i == "homma" | i == "monod" | i == "glen") {
       matrices <- c("A", "AB")
     } else {
       matrices <- c("A", "B", "AB", "BA")
@@ -153,12 +164,15 @@ lapply(ind, function(x) rbindlist(x, idcol = "Function")) %>%
              space = "free_x") +
   scale_fill_discrete(name = expression(paste("Sobol' ", T[italic(i)])),
                       labels = c("Azzini", "Homma", "Jansen", 
-                                 "Monod", "Sobol")) +
+                                 "Lamboni", "Janon / Monod", "Sobol", 
+                                 "Glen and Isaacs")) +
   labs(x = "",
        y = expression(T[italic(i)])) +
   theme_AP() + 
   theme(axis.text.x = element_text(size = 6.5), 
         legend.position = "top")
+
+
 
 
 ## ----functions_metafunction, cache=TRUE---------------------------------------------
@@ -283,7 +297,7 @@ ind <- sobol_indices(Y = Y, N = N, params = params, R = R, boot = TRUE)
 
 # DEFINE SETTINGS ------------------------------------------------------------------
 
-N <- 2 ^ 11 # Sample size of sample matrix
+N <- 2 ^ 4 # Sample size of sample matrix
 R <- 500 # Number of bootstrap replicas
 n_cores <- ceiling(detectCores() * 0.5)
 order <- "first"
@@ -322,7 +336,7 @@ mat <- as.matrix(data.table(tmp)[, (sel):= lapply(.SD, function(x)
 
 model_Ti <- function(k, N.all, N.azzini, N.high, k_2, k_3, epsilon, phi, delta) {
   ind <- list()
-  estimators <- c("jansen", "sobol", "homma", "monod", "azzini")
+  estimators <- c("jansen", "sobol", "homma", "monod", "azzini", "lamboni", "glen")
   all.but.azzini <- sobol_matrices(N = N.all, params = paste("X", 1:k, sep = ""), 
                                    matrices = c("A", "AB"))
   azzini <- sobol_matrices(N = N.azzini, params = paste("X", 1:k, sep = ""), 
@@ -342,7 +356,7 @@ model_Ti <- function(k, N.all, N.azzini, N.high, k_2, k_3, epsilon, phi, delta) 
                        total = "jansen")
   full.ind[, sample.size:= "N"]
   for(i in estimators) {
-    if(!i == "azzini") {
+    if(i == "jansen" | i == "sobol" | i == "homma" | i == "monod" | i == "glen") {
       y <- output[1:nrow(all.but.azzini)]
       n <- N.all
     } else {
@@ -407,11 +421,13 @@ mt.dt <- data.table(mat) %>%
   .[, row:= 1:.N]
 
 full_output <- merge(mt.dt, out_cor) %>%
-  .[, Nt:= ifelse(estimator == "azzini", N.azzini * (2 * k + 2), N.all * (k + 1))] %>%
+  .[, Nt:= ifelse(estimator == "azzini" | estimator == "lamboni", N.azzini * (2 * k + 2), N.all * (k + 1))] %>%
   .[, estimator:= ifelse(estimator %in% "azzini", "Azzini and Rosati", 
                         ifelse(estimator %in% "homma", "Homma and Saltelli",
                                ifelse(estimator %in% "monod", "Janon/Monod", 
-                                      ifelse(estimator %in% "jansen", "Jansen", "Sobol'"))))] %>%
+                                      ifelse(estimator %in% "jansen", "Jansen", 
+                                             ifelse(estimator %in% "glen", "Glen and Isaac", 
+                                                    ifelse(estimator %in% "lamboni", "Lamboni", "Sobol'"))))))] %>%
   .[, ratio:= Nt / k]
 
 # Define A matrix
@@ -432,6 +448,9 @@ fwrite(out, "out.csv")
 dt_median <- A[correlation > 0, .(median = median(correlation), 
                                   low.ci = quantile(correlation, 0.25), 
                                   high.ci = quantile(correlation, 0.75)), estimator]
+
+A[correlation > 0, .(quantile(correlation, c(0.01, 0.05, 0.1, 0.25, 0.5, 
+                                             0.75, 0.95, 0.99))), estimator]
 
 a <- ggplot(A[correlation > 0], aes(correlation)) +
   geom_rect(data = dt_median,
@@ -575,3 +594,163 @@ cat("Num threads: "); print(detectCores(logical = TRUE))
 ## Return the machine RAM
 cat("RAM:         "); print (get_ram()); cat("\n")
 
+
+
+sensobol::sobol_matrices()
+
+
+
+######################################
+# NEW ESTIMATORS #####################
+######################################
+
+N <- 1000
+params <- paste("X", 1:8, sep = "")
+mat <- sensobol::sobol_matrices(N = N, params = params, 
+                                matrices = c("A", "B", "AB", "BA"))
+Y <- sensobol::sobol_Fun(mat)
+
+
+k <- length(params)
+m <- matrix(Y, nrow = N)
+
+# ESTIMATE LAMBONI --------------------------------------------------
+
+# It requires an A, B, AB and BA matrices
+Y_A <- m[, 1]
+Y_B <- m[, 2]
+Y_AB <- m[, 3:(3 + k - 1)]
+Y_BA <- m[, (ncol(m) - k + 1):ncol(m)]
+f0 <- 1 / (2 * N) * sum(Y_A + Y_B)
+VY <- 1 / (2 * N - 1) * sum((Y_A - f0) ^ 2 + (Y_B - f0) ^ 2)
+
+ (1 / (4 * N) * Rfast::colsums((Y_A - Y_AB) ^ 2 + (Y_B - Y_BA) ^ 2)) / VY
+
+
+# ESTIMATE GLEN AND ISAAC -------------------------------------------
+N <- 1000
+params <- paste("X", 1:8, sep = "")
+mat <- sensobol::sobol_matrices(N = N, params = params, 
+                                matrices = c("A", "AB"))
+Y <- sensobol::sobol_Fun(mat)
+m <- matrix(Y, nrow = N)
+
+Y_A <- m[, 1]
+Y_AB <- m[, -1]
+
+
+
+1 - (1 / (N - 1) * Rfast::colsums(((Y_A - mean(Y_A)) * (Y_AB - Rfast::colmeans(Y_AB))) / 
+  sqrt(var(Y_A) * Rfast::colVars(Y_AB))))
+
+
+
+colVars()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n <- 5
+k <- 3
+mat <- randtoolbox::sobol(n = n, dim = k * 3)
+
+n <- 1000
+X1 <- data.frame(mat[, 1:k])
+X2 <- data.frame(mat[, (k + 1):(k * 2)])
+X3 <- data.frame(mat[, ((k * 2) + 1): (k * 3)])
+
+
+p <- ncol(X1)
+
+X <- rbind(X1, X2)
+for (i in 1:p) {
+  Xb <- X1
+  Xb[,i] <- X3[,i]
+  X <- rbind(X, Xb)
+}
+for (i in 1:p) {
+  Xb <- X2
+  Xb[,i] <- X1[,i]
+  X <- rbind(X, Xb)
+}
+for (i in 1:p) {
+  Xb <- X3
+  Xb[,i] <- X2[,i]
+  X <- rbind(X, Xb)
+}
+
+Y <- sensobol::ishigami_Fun(X)
+d <- as.matrix(data[1:nrow(X1), ]) # as.matrix for colSums
+n <- nrow(d)
+p <- (ncol(d)-2)/3
+
+
+
+(colSums((d[, 1] - d[, 3:(2+p)])*(d[, (3+p):(2+2*p)] - d[,2])) / n)
+
+
+
+
+
+
+data <- matrix(Y, nrow = n) 
+
+n <- nrow(d)
+p <- (ncol(d)-2)/3
+V <- numeric(0)
+for (k in 1:p) {
+  V[k] <- mean(apply(data[,c(1,2,2+k,2+k+p)]^2,1, mean))-(mean(apply(data[,c(1,2,2+k,2+k+p)],1,mean)))^2
+}
+VCE <- (colSums((data[,1] - data[, 3:(2+p)])*(data[, (3+p):(2+2*p)] - data[,2])) / n)
+VCE.compl <- V - (colSums((data[,2] - data[, (3+2*p):(2+3*p)])*(data[, (3+p):(2+2*p)] - data[,1])) / n)
+S <- V[(k + 1):(p + k), 1, drop = FALSE] / V[1:k,1]
+T <- V[(p + k + 1):(2 * p + k), 1, drop = FALSE] / V[1:k,1]
+
+# as.matrix for colSums
+n <- nrow(d)
+p <- (ncol(d)-2)/3
+
+
+
+
+# sensitivity analysis
+
+
+x <- sensitivity::sobolowen(model = sensitivity::sobol.fun, X1 = A, X2 = B, X3 = C, nboot = 100)
+print(x)
+library(sensitivity)
+
+x$T$original
